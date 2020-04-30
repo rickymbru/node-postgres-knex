7bd36a33849519b31171484cdb4c4ff313c5127e
Ricky <rickymbru@gmail.com>
2020-04-30 19:12:51 -0300

    user CRUDE completed and error handling

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,50 @@
-const knex = require ('../database/');
+const knex = require ('../database');
 
 module.exports = {
     async index (req, res) {
         const results = await knex('users');
 
         return res.json(results);
-    }
+    },
+
+    async create (req, res, next) {
+        const { username } = req.body;
+        try{
+            const user = await knex('users').insert({
+                username
+            });
+
+            return res.status(201).send();
+        } catch(error){
+            next (error)
+        }        
+    },
+
+    async update (req, res, next) {
+        try {
+            const {id} = req.params
+            const { username } = req.body;
+
+            await knex('users')
+            .update({ username })
+            .where({ id })
+
+            return res.send();
+        }catch (error){
+            next (error)
+        }
+    }, 
+    async delete (req, res, next) {
+        try {
+            const {id} = req.params
+
+            await knex('users')
+            .delete()
+            .where({ id })
+
+            return res.send();
+        }catch (error){
+            next (error)
+        }
+    },     
 }
--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
 const express = require ('express');
+
 const routes = require ('./routes');
 
+require('dotenv').config();
+
 const app = express();
+
+app.use(express.json());
 app.use(routes);
 
-require('dotenv').config();
+// Not Found
+app.use((req, res, next) =>{
+    const error = new Error('Not found')
+    error.status = 404
+    next (error)
+})
+// Catch All
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
 
 app.listen(3333 || process.env.PORT, () => console.log('Server is running'));
